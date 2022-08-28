6b64a2315c3f88c13f6026d11fb318d32975354f
cryptodeal <39316528+cryptodeal@users.noreply.github.com>
2022-08-27 20:11:12 -0500

    add unit tests for log, floor, ceil, & abs

--- test/ops.test.ts
+++ test/ops.test.ts
@@ -7,7 +7,6 @@ import {
 import {
   expectArraysClose
 } from "./utils";
-/* Run w `bun wiptest` */
 
 describe('ops', () => {
   it('should support basic math operators (add, sub, mul, div)', () => {
@@ -19,16 +18,40 @@ describe('ops', () => {
     expect(sm.div(a, b).valueOf()).toBe(2);
   });
 
-  it('should work for exp', async () => {
+  it('should work for exp', () => {
     const a = sm.tensor(new Float32Array([1, 2, 0]))
     const r = sm.exp(a);
     expect(expectArraysClose(r.valueOf(), [Math.exp(1), Math.exp(2), 1])).toBe(true);
   });
 
+  it('should work for log', () => {
+    const a = sm.tensor(new Float32Array([1, 2]))
+    const r = sm.log(a);
+    expect(expectArraysClose(r.valueOf(), [Math.log(1), Math.log(2)])).toBe(true);
+  });
+
+  it('should work for floor', async () => {
+    const a = sm.tensor(new Float32Array([1.5, 2.1, -1.4]));
+    const r = sm.floor(a);
+    expect(expectArraysClose(r.valueOf(), [1, 2, -2])).toBe(true);
+  });
+
+  it('should work for ceil', async () => {
+    const a = sm.tensor(new Float32Array([1.5, 2.1, -1.4]));
+    const r = sm.ceil(a);
+    expect(expectArraysClose(r.valueOf(), [2, 3, -1])).toBe(true);
+  });
+
+  it('should work for abs', async () => {
+    const a = sm.tensor(new Float32Array([1, -2, 0, 3, -0.1]));
+    const r = sm.abs(a);
+    expect(expectArraysClose(r.valueOf(), [1, 2, 0, 3, 0.1])).toBe(true);
+  });
+
   it('should copy', () => {
     const a = sm.tensor(new Float32Array([1, 2, 3, 4]));
     const b = a.copy();
-    const length = b.elements
+    const length = a.elements
     for (let i = 0; i < length; i++) {
       expect(a[i]).toBe(b[i]);
     }
