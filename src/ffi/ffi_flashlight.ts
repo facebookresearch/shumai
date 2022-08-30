31c4088da9a53b3f112e3642458e42139d3abfc3
jacobkahn <jacobkahn1@gmail.com>
2022-08-30 14:52:13 -0500

    rebase

--- src/ffi/ffi_flashlight.ts
+++ src/ffi/ffi_flashlight.ts
@@ -2,7 +2,9 @@ import { dlopen, suffix } from 'bun:ffi'
 import { ffi_tensor } from './ffi_tensor'
 import { ffi_tensor_ops } from './ffi_tensor_ops_gen'
 
-const path = `${__dirname}/../../libflashlight_binding.${suffix}`
+const pkgname = `@shumai/${process.platform}_${process.arch}_shumai_flashlight`
+const BINDING_NAME_PREFIX = 'libflashlight_binding'
+const path = import.meta.resolveSync(`${pkgname}/${BINDING_NAME_PREFIX}.${suffix}`)
 const { symbols: fl } = dlopen(path, {
   ...ffi_tensor,
   ...ffi_tensor_ops
