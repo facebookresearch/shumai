7d7197a688036096466b77fd1f7717b7e9af223e
Jacob Kahn <jacobkahn1@gmail.com>
2022-08-30 15:18:47 -0500

    [pkg] bun install automation with linux-x64/arm and macOS x64 (#5)
    Automate build and publication of native binaries for shumai.
    
    Adds Github Actions that'll build Flashlight from source and shumai bindings to Flashlight from source triggered by master commits across linux and macOS (amd64 for now, arm64 automation coming soon), then publish to NPM.
    
    We've decided **not** to ship binaries related to tensor libraries under Flashlight that aren't implicitly bundled; we've done this to minimize package size, which is currently < 8 MB, and would exceed 100 MB if bundling ArrayFire. The recommendation and install procedure will continue to be to use [ArrayFire's recommended approaches](https://github.com/arrayfire/arrayfire/wiki/Getting-ArrayFire) to installation before installing shumai <> Flashlight bindings.
    
    For now, macOS support is ArrayFire CPU only; **the ArrayFire CPU backend is a debugging tool and is far below peak performance**. Currently-shipped packages on linux have the ArrayFire CPU backend as well, but I'll be swapping this to the CUDA backend shortly.
    
    Flashlight support for better heterogeneous compute (e.g. CPU and GPU) is in progress with Intel's [oneDNN library](https://github.com/flashlight/flashlight/tree/main/flashlight/fl/tensor/backend/onednn), and we may eventually target dynamic backend registration in Flashlight, allowing users to install whichever platform-specific libs they want.
    
    Tricks in this implementation:
    - NPM `optionalDependencies` tries and warns if it fails to install a package -- similarly to [bun-webkit](https://github.com/oven-sh/WebKit/blob/main/.github/workflows/build.yml), we list all possible platform and arch specific packages; npm/bun will only install the one which fits the right `{os, platform}` product.
    - Native lib versioning is done via a frozen `0.0.1-${{ github.sha }}` structure -- `shumai` uses the wildcard (`*`) version to ensure npm always pull the latest version of the package. We'll be using a [**live at head**](https://abseil.io/about/philosophy#we-recommend-that-you-choose-to-live-at-head) philosophy for now regarding Flashlight integration and native language interop.
    
    This also clears the path for `bun install shumai` which will be published shortly.

--- src/ffi/ffi_flashlight.ts
+++ src/ffi/ffi_flashlight.ts
@@ -2,10 +2,33 @@ import { dlopen, suffix } from 'bun:ffi'
 import { ffi_tensor } from './ffi_tensor'
 import { ffi_tensor_ops } from './ffi_tensor_ops_gen'
 
-const path = `${__dirname}/../../libflashlight_binding.${suffix}`
-const { symbols: fl } = dlopen(path, {
-  ...ffi_tensor,
-  ...ffi_tensor_ops
-})
+const pkgname = `@shumai/${process.platform}_${process.arch}_shumai_flashlight`
+const BINDING_NAME_PREFIX = 'libflashlight_binding'
+const path = import.meta.resolveSync(`${pkgname}/${BINDING_NAME_PREFIX}.${suffix}`)
+const { symbols: fl } = (() => {
+  try {
+    return dlopen(path, {
+      ...ffi_tensor,
+      ...ffi_tensor_ops
+    })
+  } catch (error) {
+    console.error("Bun trace:", error)
+    // TODO: swap to recommending arrayfire-cuda3-cuda-11-6 once linux AF is GPU-only
+    throw new Error(`shumai was unable to load backing libraries!
+    Make sure a valid tensor backend (e.g. ArrayFire) is installed by running,
+    for example:
+      ${process.platform === 'darwin' ? 'brew install arrayfire' : ''}
+      ${process.platform === 'linux' ? 'sudo apt install arrayfire-cpu3-openblas' : ''}
+
+    or see the ArrayFire documentation
+    (https://github.com/arrayfire/arrayfire/wiki/Getting-ArrayFire)
+    for installing on your OS / distribution.
+
+    If you're still having trouble, please create an issue
+    (https://github.com/facebookresearch/shumai/issues)
+    outlining your system and platform details.
+    `);
+  }
+})()
 
 export { fl }
