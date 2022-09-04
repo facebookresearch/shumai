5fb20cb1f9a4c1379a7ce95e16b25c16288a01b6
cryptodeal <39316528+cryptodeal@users.noreply.github.com>
2022-09-04 16:47:16 -0500

    start work on eval tuner PR

---
+++ test/eval.test.ts
@@ -0,0 +1,24 @@
+import * as sm from '@shumai/shumai'
+
+import { it, describe, expect } from 'bun:test'
+import { EvalTuner } from '../src/utils/eval_tuner'
+import { logBenchmarkInfo } from './utils'
+
+describe('eval (memtest)', () => {
+  it('basic', () => {
+    const eval_tuner = new EvalTuner()
+    const a = sm.full([1024 * 8], 1)
+    const iters = 10000
+    const t0 = performance.now()
+    let b = a
+    for (let i = 0; i < iters; i++) {
+      b = b.add(a)
+      const isDone = eval_tuner.step(b, i)
+      if (isDone) break
+    }
+    const o = b.toFloat32Array()[0]
+    const t1 = performance.now()
+    expect(typeof eval_tuner.best_delta).toBe('number')
+    logBenchmarkInfo(t0, t1, iters, o)
+  })
+})
