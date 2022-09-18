4244a6ca00d6ece9bb97af9781f3516ebeb1994a
cryptodeal <39316528+cryptodeal@users.noreply.github.com>
2022-09-17 19:34:41 -0500

    fix failing transpose test

--- test/transpose.test.ts
+++ test/transpose.test.ts
@@ -9,36 +9,30 @@ describe('transpose', () => {
     expect(areSameShape(t, r)).toBe(true)
     expectArraysClose(r.toFloat32Array(), t.toFloat32Array())
   })
-
-  /* TODO: FIX - CURRENTLY FAILS */
   it('2D Tensor', () => {
     const t = sm.tensor(new Float32Array([1, 11, 2, 22, 3, 33, 4, 44])).reshape([2, 4])
-    t.transpose([1, 0])
-    expect(isShape(t, [2, 4])).toBe(true)
-    expectArraysClose(t.toFloat32Array(), [1, 3, 11, 33, 2, 4, 22, 44])
+    const t2 = t.transpose([1, 0])
+    expect(isShape(t2, [4, 2])).toBe(true)
+    expectArraysClose(t2.toFloat32Array(), [1, 3, 11, 33, 2, 4, 22, 44])
   })
-
   it('2D Tensor; shape has ones', () => {
     const t = sm.tensor(new Float32Array([1, 2, 3, 4])).reshape([1, 4])
     const t2 = sm.transpose(t, [1, 0])
     expect(isShape(t2, [4, 1])).toBe(true)
     expectArraysClose(t2.toFloat32Array(), [1, 2, 3, 4])
   })
-
   it('3D Tensor [r, c, d] => [d, r, c]', () => {
     const t = sm.tensor(new Float32Array([1, 11, 2, 22, 3, 33, 4, 44])).reshape([2, 2, 2])
     const t2 = sm.transpose(t, [2, 0, 1])
     expect(isShape(t2, [2, 2, 2])).toBe(true)
     expectArraysClose(t2.toFloat32Array(), [1, 2, 3, 4, 11, 22, 33, 44])
   })
-
   it('3D Tensor [r, c, d] => [d, c, r]', () => {
     const t = sm.tensor(new Float32Array([1, 11, 2, 22, 3, 33, 4, 44])).reshape([2, 2, 2])
     const t2 = sm.transpose(t, [2, 1, 0])
     expect(isShape(t2, [2, 2, 2])).toBe(true)
     expectArraysClose(t2.toFloat32Array(), [1, 3, 2, 4, 11, 33, 22, 44])
   })
-
   it('3D Tensor [r, c, d] => [d, r, c], shape has ones', () => {
     const perm = [2, 0, 1]
     const t = sm.tensor(new Float32Array([1, 2, 3, 4])).reshape([2, 1, 2])
@@ -56,7 +50,6 @@ describe('transpose', () => {
     expect(isShape(tt3, [2, 1, 2])).toBe(true)
     expectArraysClose(tt3.toFloat32Array(), [1, 3, 2, 4])
   })
-
   it('3D Tensor [r, c, d] => [r, d, c]', () => {
     const perm = [0, 2, 1]
     const t = sm.tensor(new Float32Array([1, 2, 3, 4, 5, 6, 7, 8])).reshape([2, 2, 2])
@@ -64,7 +57,6 @@ describe('transpose', () => {
     expect(isShape(tt, [2, 2, 2])).toBe(true)
     expectArraysClose(tt.toFloat32Array(), [1, 3, 2, 4, 5, 7, 6, 8])
   })
-
   /* TODO: FIX - CURRENTLY FAILS (Throws C++ Exception)
     it('5D Tensor [r, c, d, e, f] => [r, c, d, f, e]', () => {
       const t = sm
@@ -81,7 +73,6 @@ describe('transpose', () => {
       )
     })
   */
-
   it('4D Tensor [r, c, d, e] => [c, r, d, e]', () => {
     const t = sm
       .tensor(new Float32Array(new Array(16).fill(0).map((x, i) => i + 1)))
@@ -90,7 +81,6 @@ describe('transpose', () => {
     expect(isShape(t2, [2, 2, 2, 2])).toBe(true)
     expectArraysClose(t2.toFloat32Array(), [1, 2, 3, 4, 9, 10, 11, 12, 5, 6, 7, 8, 13, 14, 15, 16])
   })
-
   it('4D Tensor [r, c, d, e] => [c, r, e, d]', () => {
     const t = sm
       .tensor(new Float32Array(new Array(16).fill(0).map((x, i) => i + 1)))
@@ -99,7 +89,6 @@ describe('transpose', () => {
     expect(isShape(t2, [2, 2, 2, 2])).toBe(true)
     expectArraysClose(t2.toFloat32Array(), [1, 3, 2, 4, 9, 11, 10, 12, 5, 7, 6, 8, 13, 15, 14, 16])
   })
-
   it('4D Tensor [r, c, d, e] => [e, r, c, d]', () => {
     const t = sm
       .tensor(new Float32Array(new Array(16).fill(0).map((x, i) => i + 1)))
@@ -108,7 +97,6 @@ describe('transpose', () => {
     expect(isShape(t2, [2, 2, 2, 2])).toBe(true)
     expectArraysClose(t2.toFloat32Array(), [1, 3, 5, 7, 9, 11, 13, 15, 2, 4, 6, 8, 10, 12, 14, 16])
   })
-
   it('4D Tensor [r, c, d, e] => [d, c, e, r]', () => {
     const t = sm
       .tensor(new Float32Array(new Array(16).fill(0).map((x, i) => i + 1)))
@@ -117,7 +105,6 @@ describe('transpose', () => {
     expect(isShape(t2, [2, 2, 2, 2])).toBe(true)
     expectArraysClose(t2.toFloat32Array(), [1, 9, 2, 10, 5, 13, 6, 14, 3, 11, 4, 12, 7, 15, 8, 16])
   })
-
   /* TODO: FIX - CURRENTLY FAILS (Throws C++ Exception)
     it('5D Tensor [r, c, d, e, f] => [c, r, d, e, f]', () => {
       const t = sm
@@ -134,7 +121,6 @@ describe('transpose', () => {
       )
     })
   */
-
   /* TODO: FIX - CURRENTLY FAILS (Throws C++ Exception)
     it('6D Tensor [r, c, d, e, f] => [r, c, d, f, e]', () => {
       const t = sm
@@ -152,7 +138,6 @@ describe('transpose', () => {
       )
     })
   */
-
   /* TODO: FIX - CURRENTLY FAILS (Throws C++ Exception)
     it('6D Tensor [r, c, d, e, f, g] => [c, r, d, e, f, g]', () => {
       const t = sm
@@ -170,6 +155,5 @@ describe('transpose', () => {
       )
     })
   */
-
   /* TODO: unit tests for gradients */
 })
