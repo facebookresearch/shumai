8b71176ccb0d35e75427076e174c3418f9b213f4
Bram Wasti <bwasti@fb.com>
2022-09-26 12:26:31 -0400

    fix up basic network example

--- examples/client.ts
+++ examples/client.ts
@@ -1,19 +1,23 @@
 import * as sm from '@shumai/shumai'
 
-const url = 'localhost:3000'
+const url = '0.0.0.0:3000'
 const model = sm.io.remote_model(url)
 
 const ref_weight = sm.full([128, 8], 1)
+let loss
+const upd = () => {
+  if (loss) {
+    return ` loss: ${loss.toFloat32()}`
+  }
+}
 
-for (const _ of sm.util.range(200)) {
-  const input = sm.randn([16, 128])
+for (const _ of sm.util.viter(10000, upd)) {
+  const input = sm.randn([128 * 4, 128])
   const ref_output = input.matmul(ref_weight)
 
   const output = await model(input)
 
-  const loss = sm.loss.mse(output, ref_output)
+  loss = sm.loss.mse(output, ref_output)
   await loss.backward()
-
-  const learned_val = await sm.io.tfetch(`${url}`)
-  console.log(`loss ${loss.toFloat32()} learned_val[0] ${learned_val.toFloat32()}`)
+  Bun.gc(true)
 }
--- examples/serve.ts
+++ examples/serve.ts
@@ -1,23 +1,8 @@
 import * as sm from '@shumai/shumai'
 
-const X = sm.randn([128, 8]).requireGrad()
+const W = sm.randn([128, 8]).requireGrad()
+const model = (x) => {
+  return x.matmul(W)
+}
 
-sm.io.serve(
-  {
-    forward: async (u: sm.Tensor, input: sm.Tensor) => {
-      const Y = input.matmul(X)
-      // different optimizer per user
-      u.opt = async (j?: sm.Tensor) => {
-        sm.optim.sgd(await Y.backward(j), 1e-2)
-      }
-      return Y
-    },
-    optimize: (u, t: sm.Tensor) => {
-      u.opt(t)
-    },
-    default: () => {
-      return X
-    }
-  },
-  { port: 3000 }
-)
+sm.io.serve_model(model, sm.optim.sgd)
