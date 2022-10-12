c25e758a131f89b7ec865bbc7dffec0234c277cb
Aaron Silvas <aaronsilvas@gmail.com>
2022-10-12 16:37:09 -0700

    Optimize SGD (#68)

--- shumai/optim/sgd.ts
+++ shumai/optim/sgd.ts
@@ -5,8 +5,8 @@ export function sgd(
   learning_rate = 1e-3
 ) {
   const lr = sm.scalar(-learning_rate)
-  for (const k of Object.keys(grads)) {
-    const { tensor: t, grad: g } = grads[k]
+  for (const [k,v] of Object.entries(grads)) {
+    const { tensor: t, grad: g } = v
     if (t.requires_grad) {
       t.update(t.detach().add(g.detach().mul(lr)))
     }
