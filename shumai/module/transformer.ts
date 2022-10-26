489e7e048c1071a70926ce868d3364e57a088b24
Yu Shiyang <yushiyang@meta.com>
2022-10-27 03:10:46 +0800

    Renamed variables to use camelcase

--- shumai/module/transformer.ts
+++ shumai/module/transformer.ts
@@ -9,16 +9,16 @@ const sm = { ...ops, ...tensor, util }
 
 export class TransformerDotProductAttention extends Module {
   private dim: number
-  private scale_factor: Tensor
+  private scaleFactor: Tensor
 
   constructor(dim: number) {
     super()
     this.dim = dim
-    this.scale_factor = sm.scalar(1 / Math.sqrt(dim))
+    this.scaleFactor = sm.scalar(1 / Math.sqrt(dim))
   }
 
   protected scale(tensor: Tensor): Tensor {
-    return tensor.mul(this.scale_factor)
+    return tensor.mul(this.scaleFactor)
   }
 
   forward(queries: Tensor, keys: Tensor, values: Tensor): Tensor {
@@ -49,14 +49,14 @@ export class TransformerDotProductAttention extends Module {
 export class TransformerMultiheadAttention extends Module {
   private dim: number
   private heads: number
-  private attn_dim: number
-  private query_embed: Linear
-  private key_embed: Linear
-  private value_embed: Linear
+  private attentionDim: number
+  private queryEmbed: Linear
+  private keyEmbed: Linear
+  private valueEmbed: Linear
   private attention: TransformerDotProductAttention
-  private concat_embed: Linear
+  private concatEmbed: Linear
 
-  constructor(dim: number, heads: number, attn_dim?: number) {
+  constructor(dim: number, heads: number, attentionDim?: number) {
     super()
 
     if (dim % heads !== 0) {
@@ -67,16 +67,16 @@ export class TransformerMultiheadAttention extends Module {
 
     this.dim = dim
     this.heads = heads
-    if (attn_dim === undefined) {
-      this.attn_dim = dim / heads
+    if (attentionDim === undefined) {
+      this.attentionDim = dim / heads
     } else {
-      this.attn_dim = attn_dim
+      this.attentionDim = attentionDim
     }
-    this.query_embed = new Linear(dim, this.attn_dim * heads)
-    this.key_embed = new Linear(dim, this.attn_dim * heads)
-    this.value_embed = new Linear(dim, this.attn_dim * heads)
-    this.attention = new TransformerDotProductAttention(this.attn_dim)
-    this.concat_embed = new Linear(this.attn_dim * heads, dim)
+    this.queryEmbed = new Linear(dim, this.attentionDim * heads)
+    this.keyEmbed = new Linear(dim, this.attentionDim * heads)
+    this.valueEmbed = new Linear(dim, this.attentionDim * heads)
+    this.attention = new TransformerDotProductAttention(this.attentionDim)
+    this.concatEmbed = new Linear(this.attentionDim * heads, dim)
   }
 
   forward(queries: Tensor, keys: Tensor, values: Tensor): Tensor {
@@ -99,28 +99,28 @@ export class TransformerMultiheadAttention extends Module {
 
     const reshape = [...shape] // [..., tokens, dim]
     reshape[reshape.length - 1] = this.heads
-    reshape.push(this.attn_dim) // [..., tokens, heads, attn_dim]
+    reshape.push(this.attentionDim) // [..., tokens, heads, attentionDim]
 
     // Swap 2nd and 3rd last axes
     const transpose = Array.from(sm.util.range(reshape.length))
     transpose[transpose.length - 3] = transpose.length - 2
     transpose[transpose.length - 2] = transpose.length - 3
 
-    queries = this.query_embed(queries).reshape(reshape).transpose(transpose)
-    keys = this.key_embed(keys).reshape(reshape).transpose(transpose)
-    values = this.value_embed(values).reshape(reshape).transpose(transpose)
-    // embed shape [..., tokens, heads * attn_dim]
-    // reshape shape [..., tokens, heads, attn_dim]
-    // transpose shape [..., heads, tokens, attn_dim]
+    queries = this.queryEmbed(queries).reshape(reshape).transpose(transpose)
+    keys = this.keyEmbed(keys).reshape(reshape).transpose(transpose)
+    values = this.valueEmbed(values).reshape(reshape).transpose(transpose)
+    // embed shape [..., tokens, heads * attentionDim]
+    // reshape shape [..., tokens, heads, attentionDim]
+    // transpose shape [..., heads, tokens, attentionDim]
 
     const reverseTranspose = transpose
     const reverseReshape = [...shape]
-    reverseReshape[reverseReshape.length - 1] = this.heads * this.attn_dim
+    reverseReshape[reverseReshape.length - 1] = this.heads * this.attentionDim
 
-    let output = this.attention(queries, keys, values) // shape [..., heads, tokens, attn_dim]
-    output = output.transpose(reverseTranspose) // shape [..., tokens, heads, attn_dim]
-    output = output.reshape(reverseReshape) // shape [..., tokens, heads * attn_dim]
-    output = this.concat_embed(output) // shape [batch, tokens, dim]
+    let output = this.attention(queries, keys, values) // shape [..., heads, tokens, attentionDim]
+    output = output.transpose(reverseTranspose) // shape [..., tokens, heads, attentionDim]
+    output = output.reshape(reverseReshape) // shape [..., tokens, heads * attentionDim]
+    output = this.concatEmbed(output) // shape [..., tokens, dim]
 
     return output
   }
