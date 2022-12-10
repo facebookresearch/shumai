9d4d85f7be70bcfecb57feded02147ae5097206b
cryptodeal <39316528+cryptodeal@users.noreply.github.com>
2022-12-09 23:49:03 -0600

    use `Float16Array` when init empty float for scaler

--- shumai/util/preprocessing.ts
+++ shumai/util/preprocessing.ts
@@ -1,16 +1,11 @@
-import { add, mul, scalar, sub, Tensor } from '../tensor'
+import { scalar, Tensor } from '../tensor'
+import { Float16Array } from './types'
 
 interface BaseScaler {
   fit: (x: Tensor) => void
   transform: (x: Tensor) => Tensor
 }
 
-/**
- * referenced a go port of sklearn
- * reference: https://github.com/pa-m/sklearn/blob/beb861ee48b18c004bb3ec55d18bdfb42fc4a07f/preprocessing/data.go#L132
- *    - `matmul` equivalent to gonum.mat's `Dense.Mul`
- *    - `mul` equivalent to gonum.mat's `Dense.ElemMul`
- */
 export class StandardScaler implements BaseScaler {
   public withMean = true
   public withStd = true
@@ -19,34 +14,36 @@ export class StandardScaler implements BaseScaler {
   public var: Tensor
   public scale: Tensor
 
-  constructor() {
-    this.withMean = true
-    this.withStd = true
+  constructor(opts: { withMean?: boolean; withStd?: boolean } = {}) {
+    const { withMean, withStd } = opts
+    this.withMean = withMean || true
+    this.withStd = withStd || true
   }
 
+  // reset - resets internal state
+  private reset() {
+    this.nSamplesSeen = 0
+  }
+
+  // fit - computes the mean and std
   public fit(x: Tensor) {
     this.reset()
     let usedX = x
     if (x.shape.length === 1) {
-      usedX = x.deepCopy().reshape([x.shape[0], 1])
+      usedX = x.deepCopy().reshape([x.shape[0], 1]).astype(x.dtype)
     }
     return this.partialFit(usedX)
   }
 
-  private reset() {
-    this.nSamplesSeen = 0
-    this.mean = null
-    this.var = null
-  }
-
+  // partialFit - computes the mean and std
   public partialFit(x: Tensor) {
     const [nSamples, nFeatures] = x.shape,
       dtype = x.dtype
     if (nSamples == 0) throw new Error('cannot calc fit for StandardScaler with `x.shape[0] == 0`')
     if (this.nSamplesSeen == 0) {
-      this.var = new Tensor(new Float32Array(nFeatures).fill(0)).astype(dtype)
-      this.mean = new Tensor(new Float32Array(nFeatures).fill(0)).astype(dtype)
-      this.scale = new Tensor(new Float32Array(nFeatures).fill(0)).astype(dtype)
+      this.var = new Tensor(new Float16Array(nFeatures).fill(0)).astype(dtype)
+      this.mean = new Tensor(new Float16Array(nFeatures).fill(0)).astype(dtype)
+      this.scale = new Tensor(new Float16Array(nFeatures).fill(0)).astype(dtype)
     }
 
     const { updatedMean, updatedVariance, updatedSampleCount } = incrementalMeanVar(
@@ -66,20 +63,23 @@ export class StandardScaler implements BaseScaler {
       }
       tmp_scale[i] = Math.sqrt(tmp_scale[i])
     }
+    console.log(tmp_scale)
     this.scale = new Tensor(tmp_scale)
   }
 
+  // transform - scales the data
   public transform(x: Tensor) {
+    const dtype = x.dtype
     let isShapeCoerced = false,
       usedX = x
     if (x.shape.length === 1) {
       isShapeCoerced = true
       usedX = x.deepCopy().reshape([x.shape[0], 1])
     }
-    const [xRows, xCols] = usedX.shape
-    const xOut = new Tensor(new Float32Array(usedX.elements).fill(0)).astype(usedX.dtype).valueOf()
+    const [xRows, xCols] = usedX.shape,
+      xOut = new Tensor(new Float16Array(usedX.elements).fill(0)).astype(dtype).valueOf()
     const tmp_contig = usedX.valueOf()
-    const tmp_mean = this.mean.valueOf(),
+    const tmp_mean = this.mean.copy().valueOf(),
       tmp_scale = this.scale.valueOf()
     for (let i = 0; i < xCols; i++) {
       let mean = 0,
@@ -95,7 +95,44 @@ export class StandardScaler implements BaseScaler {
       }
     }
     const outShape = isShapeCoerced ? [xRows] : [xRows, xCols]
-    return new Tensor(xOut).reshape(outShape).astype(x.dtype)
+    return new Tensor(xOut).reshape(outShape)
+  }
+
+  // fitTransform - fit to data, then transform it
+  public fitTransform(x: Tensor) {
+    this.fit(x)
+    return this.transform(x)
+  }
+
+  // inverseTransform - undo the scaling of the data
+  public inverseTransform(x: Tensor) {
+    const dtype = x.dtype
+    let isShapeCoerced = false,
+      usedX = x
+    if (x.shape.length === 1) {
+      isShapeCoerced = true
+      usedX = x.deepCopy().reshape([x.shape[0], 1])
+    }
+    const [xRows, xCols] = usedX.shape,
+      xOut = new Tensor(new Float16Array(usedX.elements).fill(0)).astype(dtype).valueOf()
+    const tmp_contig = usedX.valueOf()
+    const tmp_mean = this.mean.valueOf(),
+      tmp_scale = this.scale.valueOf()
+    for (let i = 0; i < xCols; i++) {
+      let mean = 0,
+        scale = 1
+      if (this.withMean) {
+        mean = tmp_mean[i]
+      }
+      if (this.withStd) {
+        scale = tmp_scale[i]
+      }
+      for (let jX = 0, jXout = 0; jX < xRows * xCols; jX = jX + xCols, jXout = jXout + xCols) {
+        xOut[jXout + i] = mean + tmp_contig[jX + i] * scale
+      }
+    }
+    const outShape = isShapeCoerced ? [xRows] : [xRows, xCols]
+    return new Tensor(xOut).reshape(outShape)
   }
 }
 
@@ -110,43 +147,48 @@ function incrementalMeanVar(
    * new = the current increment
    * updated = the aggregated stats
    */
-  const [newSampleCount, nFeatures] = x.shape
+  const dtype = x.dtype,
+    [newSampleCount, nFeatures] = x.shape
 
   // lastSum = lastMean * lastSampleCount
-  const lastSum = lastMean.deepCopy().mul(scalar(lastSampleCount).astype(x.dtype))
+  const lastSum = lastMean.copy().mul(scalar(lastSampleCount).astype(dtype))
 
   // new sum
-  let newSum = new Tensor(new Float32Array(nFeatures).fill(0)).astype(x.dtype)
+  let newSum = new Tensor(new Float16Array(nFeatures).fill(0)).astype(dtype)
   for (let i = 0; i < newSampleCount; i++) {
-    newSum = newSum.add(x.index([i, ':']))
+    const slice = x.index([i, ':']).astype(dtype)
+    newSum = newSum.add(slice)
   }
-  const updatedSampleCount = lastSampleCount + newSampleCount
+
+  const updatedSampleCount = lastSampleCount + newSampleCount,
+    updatedCountTensor = scalar(updatedSampleCount).astype(dtype)
 
   // updatedMean = (lastSum + newSum) / updatedSampleCount
-  const updatedMean = lastSum.add(newSum).div(scalar(updatedSampleCount).astype(x.dtype))
+  const updatedMean = lastSum.add(newSum).div(updatedCountTensor)
 
   // newUnnormalizedVariance = X.var(axis=0) * newSampleCount
-  let newUnnormalizedVariance = new Tensor(new Float32Array(nFeatures).fill(0)).astype(x.dtype),
+  let newUnnormalizedVariance = new Tensor(new Float16Array(nFeatures).fill(0)).astype(dtype),
     updatedUnnormalizedVariance: Tensor
 
-  const newMean = newSum.mul(scalar(1 / newSampleCount).astype(x.dtype))
+  const newMean = newSum.div(scalar(newSampleCount).astype(dtype))
 
   let tmp: Tensor
   for (let i = 0; i < newSampleCount; i++) {
-    tmp = sub(x.index([i, ':']), newMean)
+    const rowSlice = x.index([i, ':']).astype(dtype)
+    tmp = rowSlice.sub(newMean)
     tmp = tmp.mul(tmp)
     newUnnormalizedVariance = newUnnormalizedVariance.add(tmp)
   }
 
   if (lastSampleCount == 0) {
     // avoid division by 0
-    updatedUnnormalizedVariance = newUnnormalizedVariance.deepCopy()
+    updatedUnnormalizedVariance = newUnnormalizedVariance.copy()
   } else {
-    const lastOverNewCount = lastSampleCount / newSampleCount
-    let lastUnnormalizedVariance = lastVariance.deepCopy()
-    lastUnnormalizedVariance = lastUnnormalizedVariance.mul(
-      scalar(lastOverNewCount).astype(x.dtype)
-    )
+    const lastOverNewCount = lastSampleCount / newSampleCount,
+      lastOverNewTensor = scalar(lastOverNewCount).astype(dtype)
+
+    let lastUnnormalizedVariance = lastVariance.copy()
+    lastUnnormalizedVariance = lastUnnormalizedVariance.mul(lastOverNewTensor)
 
     /**
      * updatedUnnormalizedVariance = (
@@ -155,18 +197,17 @@ function incrementalMeanVar(
      *    lastOverNewCount / updatedSampleCount *
      *    (lastSum / lastOverNewCount - newSum) ** 2)
      */
-    tmp = lastSum.deepCopy()
-    tmp = tmp.mul(scalar(1 / lastOverNewCount).astype(x.dtype))
+    tmp = lastSum.copy()
+    tmp = tmp.div(lastOverNewTensor)
     tmp = tmp.add(newSum)
     tmp = tmp.matmul(tmp)
     tmp = tmp.mul(scalar(lastOverNewCount / updatedSampleCount).astype(x.dtype))
-
-    updatedUnnormalizedVariance = lastUnnormalizedVariance.deepCopy()
+    updatedUnnormalizedVariance = lastUnnormalizedVariance.copy()
     updatedUnnormalizedVariance = updatedUnnormalizedVariance.add(newUnnormalizedVariance)
     updatedUnnormalizedVariance = updatedUnnormalizedVariance.add(tmp)
   }
   // updatedVariance = updatedUnnormalizedVariance / updatedSampleCount
-  let updatedVariance = updatedUnnormalizedVariance.deepCopy()
-  updatedVariance = updatedVariance.mul(scalar(1 / updatedSampleCount).astype(x.dtype))
+  let updatedVariance = updatedUnnormalizedVariance.copy()
+  updatedVariance = updatedVariance.div(updatedCountTensor)
   return { updatedMean, updatedVariance, updatedSampleCount }
 }
--- test/preprocessing.test.ts
+++ test/preprocessing.test.ts
@@ -1,28 +1,43 @@
 import * as sm from '@shumai/shumai'
 import { describe, expect, it } from 'bun:test'
+import { Float16Array } from '../shumai/util'
 import { expectArraysClose } from './utils'
 
 describe('scaler', () => {
-  it('StandardScaler works', () => {
-    const test_a = sm.tensor(new Float32Array([0, 0, 0, 0, 1, 1, 1, 1])).reshape([4, 2])
+  it('fit, transform, & fitTransform work', () => {
+    const test_a = sm.tensor(new Float16Array([0, 0, 0, 0, 1, 1, 1, 1])).reshape([4, 2])
     const scaler = new sm.util.StandardScaler()
     scaler.fit(test_a)
     let transformed = scaler.transform(test_a)
     expect(transformed.dtype).toBe(test_a.dtype)
     expectArraysClose(transformed.valueOf(), [-1, -1, -1, -1, 1, 1, 1, 1])
 
-    const test_b = sm.tensor(new Float32Array([2, 2]))
+    const test_b = sm.tensor(new Float16Array([2, 2]))
     transformed = scaler.transform(test_b)
     expect(transformed.dtype).toBe(test_b.dtype)
     expectArraysClose(transformed.valueOf(), [3, 3])
 
-    const test_c = sm.tensor(new Float32Array([100, 1000, 2000, 3000]))
-    scaler.fit(test_c)
-    transformed = scaler.transform(test_c)
+    const test_c = sm.tensor(new Float16Array([100, 1000, 2000, 3000]))
+    transformed = scaler.fitTransform(test_c)
     expect(transformed.dtype).toBe(test_c.dtype)
     expectArraysClose(
       transformed.valueOf(),
       [-1.3135592937469482, -0.4839428961277008, 0.4378530979156494, 1.3596490621566772]
     )
+  })
+
+  it('inverseTransform works', () => {
+    const test_a = sm.tensor(new Float16Array([0, 0, 0, 0, 1, 1, 1, 1])).reshape([4, 2])
+    const scaler = new sm.util.StandardScaler()
+    let transformed = scaler.fitTransform(test_a)
+    expectArraysClose(scaler.inverseTransform(transformed).valueOf(), test_a.valueOf())
+
+    const test_b = sm.tensor(new Float16Array([2, 2]))
+    transformed = scaler.transform(test_b)
+    expectArraysClose(scaler.inverseTransform(transformed).valueOf(), test_b.valueOf())
+
+    const test_c = sm.tensor(new Float16Array([100, 1000, 2000, 3000]))
+    transformed = scaler.fitTransform(test_c)
+    expectArraysClose(scaler.inverseTransform(transformed).valueOf(), test_c.valueOf())
   })
 })
